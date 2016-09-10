0b5d30b67cf68bf37bc0aa1718e406bb75ff2539
MAC <mac@Macs-MacBook-Pro.local>
2016-09-10 16:14:35 +0800

    This my first commit Changed to tapcount to 10

--- Helloword/ViewController.swift
+++ Helloword/ViewController.swift
@@ -10,11 +10,28 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var theLabel: UILabel!
+    
+    var tapCount = 10
+    
+    @IBAction func buttonTapped(sender: AnyObject) {
+        
+    tapCount = tapCount + 1
+        
+        if tapCount >= 10 {
+        
+        theLabel.text = "Tapped 10 times"
+
+        }
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
     }
-
+    
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
